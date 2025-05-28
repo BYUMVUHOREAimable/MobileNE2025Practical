eb13cd85315f4efb010068fad97cd2e24dbfba12
BYUMVUHORE Aimable <aimablebyumvuhore@gmail.com>
2025-05-28 11:41:19 +0200

    complete project

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { AuthProvider } from './state/AuthContext';
+import AppNavigator from './navigation/AppNavigator';
+import { LogBox } from 'react-native';
+
+// Optional: Ignore specific warnings if they are known and not critical
+LogBox.ignoreLogs(['Setting a timer']); // Example
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <AppNavigator />
+    </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
